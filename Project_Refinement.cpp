ce60485cfb30c49a3d178762436a2ab9b7cc09c6
EVSNHU <fernando.ventura@snhu.edu>
2026-01-31 20:16:47 -0800

    Delete Project_Refinement.cpp

--- Project_Refinement.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Project_Refinement.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_Refinement, "Project_Refinement" );
